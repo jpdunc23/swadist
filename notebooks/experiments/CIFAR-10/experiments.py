97d7188be0aa3c6a995e59fc85d64d583374c23f
James Duncan <jpduncan@berkeley.edu>
2022-03-07 09:17:20 -0800

    Add remaining CIFAR-10 experiments

--- notebooks/experiments/CIFAR-10/experiments.py
+++ notebooks/experiments/CIFAR-10/experiments.py
@@ -27,7 +27,8 @@ def main(rank, world_size, batch_size, lr0, momentum, exper, datadir, rundir, st
 
     # pin process to a single device
     torch.cuda.set_device(rank)
-    model = ResNet(in_kernel_size=3, stack_sizes=[1, 1, 1], n_classes=10, batch_norm=False).to(rank)
+    model = ResNet(in_kernel_size=3, stack_sizes=[1, 1, 1], n_classes=10,
+                   batch_norm=False).to(rank)
 
     # epochs, scaling epochs, decay factor
     epochs, T, alpha = 25, 20, 0.25
@@ -35,10 +36,9 @@ def main(rank, world_size, batch_size, lr0, momentum, exper, datadir, rundir, st
     optimizer = torch.optim.SGD(model.parameters(), lr=lr0, momentum=momentum, nesterov=True)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    self.writer = SummaryWriter(f'{log_dir}/{exper}_{timestamp}')
     # load training and validation data
-    train_loader, valid_loader = get_dataloaders('cifar10', rootdir=datadir, test=False, batch_size=batch_size)
+    train_loader, valid_loader = get_dataloaders('cifar10', rootdir=datadir,
+                                                 test=False, batch_size=batch_size)
 
     # setup optimizer / lr sched
     lr_lambda = lambda epoch: 1 - (1 - alpha)*epoch/T if epoch < T else (alpha if epoch < epochs else lr0)
@@ -50,16 +50,33 @@ def main(rank, world_size, batch_size, lr0, momentum, exper, datadir, rundir, st
                       log_dir=rundir, name=exper, device=rank, rank=rank,
                       world_size=world_size)
 
-    if exper == 'codist':
-        trainer.train(train_loader, valid_loader, epochs=5, codist_epochs=epochs-5,
+    # train
+    if exper == 'sgd':
+        trainer.train(train_loader, valid_loader, epochs=epochs,
                       stopping_acc=stopping_acc, validations_per_epoch=4)
         n_epochs = trainer.total_train_epochs
 
-    elif exper == 'swadist':
-        trainer.train(train_loader, valid_loader, epochs=5, codist_epochs=epochs-10,
+
+    if exper == 'swa':
+        trainer.train(train_loader, valid_loader, epochs=epochs-5,
                       swa_epochs=5, stopping_acc=stopping_acc,
                       validations_per_epoch=4)
+        n_epochs = trainer.total_train_epochs
 
+
+    if exper == 'codist':
+        trainer.train(train_loader, valid_loader, epochs=5,
+                      codist_epochs=epochs-5, stopping_acc=stopping_acc,
+                      validations_per_epoch=4)
+        n_epochs = trainer.total_train_epochs
+
+
+    elif exper == 'swadist':
+        trainer.train(train_loader, valid_loader, epochs=5,
+                      codist_epochs=epochs-10, swa_epochs=5,
+                      stopping_acc=stopping_acc, validations_per_epoch=4)
+
+    # record metrics
     train_loss = trainer.train_losses[n_epochs]
     valid_loss = trainer.valid_losses[n_epochs]
     train_acc = trainer.train_accs[n_epochs]
@@ -93,9 +110,9 @@ if __name__ == "__main__":
 
     # initial lr, scaling factor, momentum
     # lr0 = 2**np.array([-8.5, -12.5, -5., -8.5, -7., -5., -5., -5., -6., -5, -7, -4, -6])
-    lr0 = 2**np.array([-8.5, -12.5, -5., -8.5, -7., -5., -5., -5., -6., -5, -7, -4, -6])
+    lr0 = 2**np.array([-5., -5., -5., -6., -5, -7, -4, -6])
     # momentum = [.675, .98, .63, .97, .975, .95, .97, .975, .98, .975, .98, .97, .975]
-    momentum = [.675, .98, .63, .97, .975, .95, .97, .975, .98, .975, .98, .97, .975]
+    momentum = [.95, .97, .975, .98, .975, .98, .97, .975]
 
     assert len(lr0) == len(batch_size), 'lr0 has the wrong size'
     assert len(momentum) == len(momentum), 'momentum has the wrong size'
